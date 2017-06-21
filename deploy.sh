bf9fc0420fb922d498789db1b469b014a1a22f6c
vankesteren <erikjan.vankesteren@gmail.com>
2017-06-21 23:22:16 +0200

    Deploy tryout

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-USERNAME=aaaa
-PASSWD=bbbb
-UPLOAD_URL=http://example.com/dav/
+USERNAME=$1
+PASSWD=$2
+UPLOAD_URL=$3
 
-BIN32=out/make/*_i386.deb
-BIN64=out/make/*_amd64.deb
 TARGET32=Guitzli_setup_32.deb
 TARGET64=Guitzli_setup_64.deb
 
 echo uploading 32bit binary
-curl -u $USERNAME:$PASSWD -T $BIN32 "$UPLOAD_URL$TARGET32" 
+curl -u $USERNAME:$PASSWD -T out/make/*_i386.deb "$UPLOAD_URL$TARGET32" 
 
 echo uploading 64bit binary
-curl -u $USERNAME:$PASSWD -T $BIN64 "$UPLOAD_URL$TARGET64"
+curl -u $USERNAME:$PASSWD -T out/make/*_amd64.deb "$UPLOAD_URL$TARGET64"
 
 echo upload complete
